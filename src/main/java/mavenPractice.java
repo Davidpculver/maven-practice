34626896c4fc0390ba11378cfcd2e5cb2d1cd6ac
David Culver <culver.david.p@gmail.com>
2021-06-14 17:23:40 -0500

    completed maven walkthrough

--- src/main/java/mavenPractice.java
+++ src/main/java/mavenPractice.java
@@ -10,15 +10,24 @@ public class mavenPractice {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please your favorite quote.");
         String resp = scanner.nextLine();
+        System.out.println("You Entered: " + resp);
 
+//        My solution: checks if there is a number anywhere in the input
         if(StringUtils.containsAny(resp, "1234567890")){
             System.out.println("You entered a string that contains a number.");
         }else{
             System.out.println("Your quote does not contain any numbers.");
         };
 
-        System.out.println(StringUtils.swapCase(resp));
-        System.out.println(StringUtils.reverse(resp));
+//        instructor example
+//        if(!StringUtils.isNumeric(resp)){
+//            System.out.println("This is not a number");
+//        } else {
+//            System.out.println("This is a number.");
+//        };
+
+        System.out.println("Flipped Case: " + StringUtils.swapCase(resp));
+        System.out.println("Reversed: " + StringUtils.reverse(resp));
 
 
     }
